9ae92fa80b7a543d861b2b26a082339bb0cb310b
Cristian Frincu <Crisitan@MacBook-Pro-Cristian.local>
2016-10-03 13:48:39 -0400

    Separated the robot classes and now working on adding a histogram functionality to show the weights

--- RoboSim2.py
+++ RoboSim2.py
@@ -9,6 +9,10 @@
 from math import *
 import random
 import matplotlib.pyplot as plt
+import numpy as np
+
+from RobotClass import RobotClass
+
 
 
 # landmarks which can be sensed by the robot (in meters)
@@ -19,129 +23,7 @@ landmarks = [[20.0, 20.0], [20.0, 80.0], [20.0, 50.0],
 
 # size of one dimension (in meters)
 world_size = 100.0
-wordSize = 100.0
 
-
-class RobotClass:
-    """ Class for the robot model used in this demo """
-
-    def __init__(self):
-
-        self.x = random.random() * world_size           # robot's x coordinate
-        self.y = random.random() * world_size           # robot's y coordinate
-        self.orientation = random.random() * 2.0 * pi   # robot's orientation
-
-        self.forward_noise = 0.0   # noise of the forward movement
-        self.turn_noise = 0.0      # noise of the turn
-        self.sense_noise = 0.0     # noise of the sensing
-
-
-    def set(self, new_x, new_y, new_orientation):
-        """ Set robot's initial position and orientation
-        :param new_x: new x coordinate
-        :param new_y: new y coordinate
-        :param new_orientation: new orientation
-        """
-
-        if new_x < 0 or new_x >= world_size:
-            raise ValueError('X coordinate out of bound')
-        if new_y < 0 or new_y >= world_size:
-            raise ValueError('Y coordinate out of bound')
-        if new_orientation < 0 or new_orientation >= 2 * pi:
-            raise ValueError('Orientation must be in [0..2pi]')
-
-        self.x = float(new_x)
-        self.y = float(new_y)
-        self.orientation = float(new_orientation)
-
-
-    def set_noise(self, new_forward_noise, new_turn_noise, new_sense_noise):
-        """ Set the noise parameters, changing them is often useful in particle filters
-        :param new_forward_noise: new noise value for the forward movement
-        :param new_turn_noise:    new noise value for the turn
-        :param new_sense_noise:  new noise value for the sensing
-        """
-
-        self.forward_noise = float(new_forward_noise)
-        self.turn_noise = float(new_turn_noise)
-        self.sense_noise = float(new_sense_noise)
-
-
-    def sense(self):
-        """ Sense the environment: calculate distances to landmarks
-        :return measured distances to the known landmarks
-        """
-
-        z = []
-
-        for i in range(len(landmarks)):
-            dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
-            dist += random.gauss(0.0, self.sense_noise)
-            z.append(dist)
-
-        return z
-
-
-    def move(self, turn, forward):
-        """ Perform robot's turn and move
-        :param turn:    turn command
-        :param forward: forward command
-        :return robot's state after the move
-        """
-
-        if forward < 0:
-            raise ValueError('Robot cannot move backwards')
-
-        # turn, and add randomness to the turning command
-        orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
-        orientation %= 2 * pi
-
-        # move, and add randomness to the motion command
-        dist = float(forward) + random.gauss(0.0, self.forward_noise)
-        x = self.x + (cos(orientation) * dist)
-        y = self.y + (sin(orientation) * dist)
-
-        # cyclic truncate
-        x %= world_size
-        y %= world_size
-
-        # set particle
-        res = RobotClass()
-        res.set(x, y, orientation)
-        res.set_noise(self.forward_noise, self.turn_noise, self.sense_noise)
-
-        return res
-
-
-    @staticmethod
-    def gaussian(mu, sigma, x):
-        """ calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
-        :param mu:    distance to the landmark
-        :param sigma: standard deviation
-        :param x:     distance to the landmark measured by the robot
-        :return gaussian value
-        """
-
-        # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
-        return exp(- ((mu - x) ** 2) / (sigma ** 2) / 2.0) / sqrt(2.0 * pi * (sigma ** 2))
-
-
-    def measurement_prob(self, measurement):
-        """ Calculate the measurement probability: how likely a measurement should be
-        :param measurement: current measurement
-        :return probability
-        """
-
-        prob = 1.0
-
-        for i in range(len(landmarks)):
-            dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
-            prob *= self.gaussian(dist, self.sense_noise, measurement[i])
-        return prob
-
-
-    def __repr__(self):
-        return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
 
 
 def evaluation(r, p):
@@ -236,8 +118,7 @@ def main():
     z = myrobot.sense()
 
     RESAMPLE=True
-    number_of_particles = 1000
-    starting_num_of_particles=1000
+    number_of_particles = 50
     list_of_particles = []    
 
 
@@ -249,6 +130,7 @@ def main():
         for i in range(number_of_particles):
             x = RobotClass()
             x.set_noise(0.05, 0.05, 5.0)
+            # x.set_noise(0.5, 0.25, 5.0)
             list_of_particles.append(x)
 
 
@@ -270,6 +152,17 @@ def main():
         for i in range(number_of_particles):
             weights.append(list_of_particles[i].measurement_prob(measurment))
 
+        normalized_weights=[]
+        max_weight = max(weights)
+        for weight in weights:
+            
+            normalized_weights.append(weight/max_weight)
+
+        print normalized_weights
+        plt.hist(normalized_weights, bins='auto')
+        plt.title("Histogram")
+        plt.show()
+
         measurement_ratio = min(weights)/max(weights)
 
         # resampling with a sample probability proportional to the importance weight
@@ -277,7 +170,6 @@ def main():
         #the same particles
         resampled_particles = []
         if RESAMPLE:
-
             index = int(random.random() * number_of_particles)
             beta = 0.0
             mw = max(weights)
@@ -291,26 +183,15 @@ def main():
                 resampled_particles.append(list_of_particles[index])
             list_of_particles = resampled_particles
         else:
-            index = int(random.random() * number_of_particles)
-            beta = 0.0
-            mw = max(weights)
-
-            particles_to_keep = int(number_of_particles*random.random()*1.5)
-
-            for i in range(particles_to_keep):
-                beta += random.random() * 2.0 * mw
-
-                while beta > weights[index]:
-                    beta -= weights[index]
-                    index = (index + 1) % number_of_particles
-                resampled_particles.append(list_of_particles[index])
-            list_of_particles = resampled_particles
+            #When we ignore the resampling step, we can just pass the same
+            #particles forward to the next step
+            resampled_particles = list_of_particles
 
         number_of_particles = len(list_of_particles)
 
-
         # visualize the current step
         visualization(myrobot, step, moved_particles, resampled_particles, weights)
+
 
         if PARTICLE_NUM_CHANGE_DEPENDENT:
             #This method of cutting down on the number of particles takes into account 
@@ -357,18 +238,21 @@ def main():
                 else:
                     Neff += 1/(n_weight**2)
 
-            if Neff < starting_num_of_particles/2:
-                RESAMPLE = True
-            else:
-                RESAMPLE = False
+            #For resting just resample at all times
+            # if Neff > starting_num_of_particles*20:
+            #     RESAMPLE = True
+            # else:
+            #     RESAMPLE = False
 
-        print 'Step = ', step, ', Ratio = ', measurement_ratio,", Length = ", len(list_of_particles), "Evaluation:", evaluation(myrobot,resampled_particles),"Neff:",Neff,"RESAMPLE:",RESAMPLE
+        print 'Step = ', step, ', Ratio = ', measurement_ratio,", Length = ", len(list_of_particles), "Evaluation:", evaluation(myrobot,resampled_particles)
 
 if __name__ == "__main__":
     MINIMUM_PARTICLES=10
     PARTICLE_NUM_CHANGE_DEPENDENT = False
     PARTICLE_NUM_STEP_DEPENDENT = False
     PARTICLE_NUM_SPLIT = False
-    PARTICLE_NUM_GMAPPING=True
+    PARTICLE_NUM_GMAPPING=False
+
+    RESAMPLE = True
 
     main()
---
+++ RobotClass.py
@@ -0,0 +1,134 @@
+from math import *
+import random
+
+
+# landmarks which can be sensed by the robot (in meters)
+landmarks = [[20.0, 20.0], [20.0, 80.0], [20.0, 50.0],
+             [50.0, 20.0], [50.0, 80.0], [80.0, 80.0],
+             [80.0, 20.0], [80.0, 50.0], [85.0, 20.0],
+             [20.0, 15.0]]
+
+# size of one dimension (in meters)
+world_size = 100.0
+
+
+class RobotClass:
+    """ Class for the robot model used in this demo """
+
+    def __init__(self):
+
+        self.x = random.random() * world_size           # robot's x coordinate
+        self.y = random.random() * world_size           # robot's y coordinate
+        self.orientation = random.random() * 2.0 * pi   # robot's orientation
+
+        self.forward_noise = 0.0   # noise of the forward movement
+        self.turn_noise = 0.0      # noise of the turn
+        self.sense_noise = 0.0     # noise of the sensing
+
+
+    def set(self, new_x, new_y, new_orientation):
+        """ Set robot's initial position and orientation
+        :param new_x: new x coordinate
+        :param new_y: new y coordinate
+        :param new_orientation: new orientation
+        """
+
+        if new_x < 0 or new_x >= world_size:
+            raise ValueError('X coordinate out of bound')
+        if new_y < 0 or new_y >= world_size:
+            raise ValueError('Y coordinate out of bound')
+        if new_orientation < 0 or new_orientation >= 2 * pi:
+            raise ValueError('Orientation must be in [0..2pi]')
+
+        self.x = float(new_x)
+        self.y = float(new_y)
+        self.orientation = float(new_orientation)
+
+
+    def set_noise(self, new_forward_noise, new_turn_noise, new_sense_noise):
+        """ Set the noise parameters, changing them is often useful in particle filters
+        :param new_forward_noise: new noise value for the forward movement
+        :param new_turn_noise:    new noise value for the turn
+        :param new_sense_noise:  new noise value for the sensing
+        """
+
+        self.forward_noise = float(new_forward_noise)
+        self.turn_noise = float(new_turn_noise)
+        self.sense_noise = float(new_sense_noise)
+
+
+    def sense(self):
+        """ Sense the environment: calculate distances to landmarks
+        :return measured distances to the known landmarks
+        """
+
+        z = []
+
+        for i in range(len(landmarks)):
+            dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
+            dist += random.gauss(0.0, self.sense_noise)
+            z.append(dist)
+
+        return z
+
+
+    def move(self, turn, forward):
+        """ Perform robot's turn and move
+        :param turn:    turn command
+        :param forward: forward command
+        :return robot's state after the move
+        """
+
+        if forward < 0:
+            raise ValueError('Robot cannot move backwards')
+
+        # turn, and add randomness to the turning command
+        orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
+        orientation %= 2 * pi
+
+        # move, and add randomness to the motion command
+        dist = float(forward) + random.gauss(0.0, self.forward_noise)
+        x = self.x + (cos(orientation) * dist)
+        y = self.y + (sin(orientation) * dist)
+
+        # cyclic truncate
+        x %= world_size
+        y %= world_size
+
+        # set particle
+        res = RobotClass()
+        res.set(x, y, orientation)
+        res.set_noise(self.forward_noise, self.turn_noise, self.sense_noise)
+
+        return res
+
+
+    @staticmethod
+    def gaussian(mu, sigma, x):
+        """ calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
+        :param mu:    distance to the landmark
+        :param sigma: standard deviation
+        :param x:     distance to the landmark measured by the robot
+        :return gaussian value
+        """
+
+        # calculates the probability of x for 1-dim Gaussian with mean mu and var. sigma
+        return exp(- ((mu - x) ** 2) / (sigma ** 2) / 2.0) / sqrt(2.0 * pi * (sigma ** 2))
+
+
+    def measurement_prob(self, measurement):
+        """ Calculate the measurement probability: how likely a measurement should be
+        :param measurement: current measurement
+        :return probability
+        """
+
+        prob = 1.0
+
+        for i in range(len(landmarks)):
+            dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
+            prob *= self.gaussian(dist, self.sense_noise, measurement[i])
+        return prob
+
+
+    def __repr__(self):
+        return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
