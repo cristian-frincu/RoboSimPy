1d4b5555b5a9f0655d8a1e1ac92cb053b5d74a89
Cristian Frincu <Crisitan@MacBook-Pro-Cristian.local>
2016-10-05 01:34:07 -0400

    created lankmark class

---
+++ LandmarkClass.py
@@ -0,0 +1,28 @@
+class LandmarkClass:
+	# A class that represents a landmark to a robot
+	# This class is to be used by RobotClass to create a 
+	# map using landmarks
+	def __init__(self):
+		#Absolute truth where the landmark is
+		self.real_x=0.0
+		self.real_y=0.0
+
+
+		#Where the robot believes the landmark 
+		#is located
+		self.believed_x=0.0
+		self.believed_y=0.0
+
+		#A holder where the robot can use to keep
+		#adding errorous values to, which will edventually
+		#be averaged to get a better estimate of the location
+		# of landmark
+		self.measurment_sum_x=0.0
+		self.measurment_sum_y=0.0
+		
+		#Since we are taking the distance measurments from the 
+		# robot, and the robot is assumed to be at 0,0 at all 
+		# times, we will get coordinates to the robot. Those
+		# would be stored in here
+		self.distance_to_robot_x=0.0
+		self.distance_to_robot_y=0.0
